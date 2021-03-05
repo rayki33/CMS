28291534ea18754d76161419a3d54593094087d8
Ray Chan <rayki33@gmail.com>
2021-03-05 18:06:08 +0800

    add description in english

--- wc.py
+++ wc.py
@@ -1,5 +1,6 @@
 from woocommerce import API
-from settings import CONSUMER_KEY, CONSUMER_SECRET
+from settings import CONSUMER_KEY, CONSUMER_SECRET, WEBSITE_URL
+from bs4 import BeautifulSoup
 
 wcapi = API(
     url="https://www.thecsdesign.com",
@@ -16,14 +17,14 @@ data = wcapi.get(f'products/{product_id}', params={"lang": "en"}).json()
 name = data["name"]
 description = data["description"]
 
-print(name)
+# print(name)
 
 images = data["images"]
 # for image in images:
-    # print(image["src"])
+# print(image["src"])
 
 image_1 = images[0]["src"]
-print(image_1)
+# print(image_1)
 
 import requests
 
@@ -31,30 +32,77 @@ api_key = 'acc_0dd6492e5c12fae'
 api_secret = '7af065628a007809449698cc5cdf74f3'
 image_url = image_1
 
-## tags ##
-response = requests.get(
-    'https://api.imagga.com/v2/tags?image_url=%s' % image_url,
-    auth=(api_key, api_secret))
 
-image_tags = response.json()["result"]["tags"]
-print(image_tags[0],image_tags[1])
+def tags():
+    ## tags ##
+    global api_key, api_secret, image_url
+    response = requests.get(
+        'https://api.imagga.com/v2/tags?image_url=%s' % image_url,
+        auth=(api_key, api_secret))
 
-# for tag in image_tags:
-#     print(tag["tag"])
+    image_tags = response.json()["result"]["tags"]
+    print(image_tags[0], image_tags[1])
 
-
-## color ##
-response = requests.get(
-    'https://api.imagga.com/v2/colors?image_url=%s' % image_url,
-    auth=(api_key, api_secret))
-
-colors = response.json()["result"]["colors"]["image_colors"]
-for color in colors:
-    print(round(color["percent"], 2),"% : ", color["closest_palette_color"])
+    # for tag in image_tags:
+    #     print(tag["tag"])
 
 
-## clean up HTML ##
-# from bs4 import BeautifulSoup
-#
-# clean_description = BeautifulSoup(description, "lxml").text
-# print(clean_description)
+def color():
+    ## color ##
+    global api_key, api_secret, image_url
+    response = requests.get(
+        'https://api.imagga.com/v2/colors?image_url=%s' % image_url,
+        auth=(api_key, api_secret))
+
+    colors = response.json()["result"]["colors"]["image_colors"]
+    for color in colors:
+        print(round(color["percent"], 2), "% : ", color["closest_palette_color"])
+
+
+def auth_product(product_id, language=""):
+    time_out = 20
+    language = language + "/"
+    api_code = f"{language}wc-api/v3/"
+    id = product_id
+    response = requests.get(
+        f"{WEBSITE_URL}{api_code}products/{id}?consumer_key={CONSUMER_KEY}&consumer_secret={CONSUMER_SECRET}", timeout=time_out
+    )
+    return response
+
+
+def title(product_id, language=""):
+    response = auth_product(product_id, language)
+    title = response.json()["product"]["title"]
+    return (title)
+
+
+def description(product_id, language=""):
+    response = auth_product(product_id, language)
+    description_html = response.json()["product"]["description"]
+
+    clean_description = clean_html(description_html)
+    return (clean_description)
+
+
+def clean_html(html_code):
+    ## clean up HTML ##
+
+
+    clean_text = BeautifulSoup(html_code, "lxml").text
+    return clean_text
+
+
+# print(title(3134, "en"))
+# print(description(3134,"en"))
+
+def get_english_title():
+    respond = requests.get("https://www.thecsdesign.com/en/product/?p=3134").content
+
+    soup = BeautifulSoup(respond, 'html.parser')
+
+    title_html = soup.find_all('h1', attrs={'class': 'product_title'})[0]
+    # title = BeautifulSoup(title_html[0], "lxml").text
+
+    return title_html
+
+print(get_english_title())
