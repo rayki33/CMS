8b924309962d89f698ecec5ab99c64eda409e1d9
Ray Chan <rayki33@gmail.com>
2021-03-09 17:59:56 +0800

    amend list_all_products

--- wc.py
+++ wc.py
@@ -2,6 +2,10 @@ from woocommerce import API
 from settings import CONSUMER_KEY, CONSUMER_SECRET, WEBSITE_URL
 from bs4 import BeautifulSoup
 from math import *
+import requests
+
+api_key = 'acc_0dd6492e5c12fae'
+api_secret = '7af065628a007809449698cc5cdf74f3'
 
 wcapi = API(
     url="https://www.thecsdesign.com",
@@ -12,11 +16,11 @@ wcapi = API(
     timeout=20
 )
 
+
 def get_image_url(product_id):
     data = wcapi.get(f'products/{product_id}').json()
 
     images = data["images"]
-    image = images[0]["src"]
     image_src = []
 
     for image in images:
@@ -24,15 +28,10 @@ def get_image_url(product_id):
 
     return image_src
 
-import requests
-
-api_key = 'acc_0dd6492e5c12fae'
-api_secret = '7af065628a007809449698cc5cdf74f3'
-
 
 def get_tags_by_image(product_id):
-    ## tags ##
-    global api_key, api_secret, image_url
+    # tags #
+    global api_key, api_secret
     response = requests.get(
         'https://api.imagga.com/v2/tags?image_url=%s' % get_image_url(product_id)[0],
         auth=(api_key, api_secret))
@@ -45,25 +44,26 @@ def get_tags_by_image(product_id):
     #     print(tag["tag"])
 
 
-def color():
-    ## color ##
-    global api_key, api_secret, image_url
+def color(product_id):
+    # color #
+    global api_key, api_secret
+    image_url = get_image_url(product_id)
     response = requests.get(
         'https://api.imagga.com/v2/colors?image_url=%s' % image_url,
         auth=(api_key, api_secret))
 
     colors = response.json()["result"]["colors"]["image_colors"]
-    for color in colors:
-        print(round(color["percent"], 2), "% : ", color["closest_palette_color"])
+    for predicted_color in colors:
+        print(round(predicted_color["percent"], 2), "% : ", predicted_color["closest_palette_color"])
 
 
 def auth_product(product_id, language=""):
     time_out = 20
     language = language + "/"
     api_code = f"{language}wc-api/v3/"
-    id = product_id
     response = requests.get(
-        f"{WEBSITE_URL}{api_code}products/{id}?consumer_key={CONSUMER_KEY}&consumer_secret={CONSUMER_SECRET}", timeout=time_out
+        f"{WEBSITE_URL}{api_code}products/{product_id}?consumer_key={CONSUMER_KEY}&consumer_secret={CONSUMER_SECRET}",
+        timeout=time_out
     )
     return response
 
@@ -71,7 +71,7 @@ def auth_product(product_id, language=""):
 def get_title_json(product_id, language=""):
     response = auth_product(product_id, language)
     title = response.json()["product"]["title"]
-    return (title)
+    return title
 
 
 def get_description(product_id, language=""):
@@ -79,19 +79,17 @@ def get_description(product_id, language=""):
     description_html = response.json()["product"]["description"]
 
     clean_description = clean_html(description_html)
-    return (clean_description)
+    return clean_description
 
 
 def clean_html(html_code):
-    ## clean up HTML ##
-
+    # clean up HTML #
 
     clean_text = BeautifulSoup(html_code, "lxml").text
     return clean_text
 
 
-
-def get_title(product_id, language = ""):
+def get_title(product_id, language=""):
     respond = requests.get(f"{WEBSITE_URL}{language}/product/?p={product_id}").content
     soup = BeautifulSoup(respond, 'html.parser')
     title = soup.select('h1.product_title')[0].text.strip()
@@ -99,7 +97,6 @@ def get_title(product_id, language = ""):
 
 
 def get_tags(product_id):
-
     tags = []
 
     for tag in get_tags_by_image(product_id):
@@ -120,12 +117,36 @@ def get_tags(product_id):
 def list_all_products():
     global wcapi
     page = 1
-    all_products = []
+    product_list = []
+    per_page_item = 2  # 1 to 100
 
-    products = wcapi.get(f"products?per_page=100&page={page}").json()
-    for product in products:
-        all_products.append([product["id"], product["name"]])
+    products = wcapi.get(f"products?per_page={per_page_item}&page={page}").json()
 
-    return all_products
+    while products and page == 1:
+        if products:
+            for p in products:
+                name_en = get_title(p["id"], "en")
+                product_list.append([p["id"], p["sku"], p["name"], name_en])
+                print(product_list[-1])
 
-print(list_all_products())
+        page += 1
+        products = wcapi.get(f"products?per_page={per_page_item}&page={page}").json()
+
+    return product_list
+
+
+all_products = list_all_products()
+with open('result.txt', 'w') as f:
+    for p in all_products:
+        f.write(str(p[0]),",")
+
+
+# with open('result.txt', 'w') as f:
+#     for product in all_products:
+#         f.newlines(product[0])
+
+# all_products = [[1,"A"],[2,"B"],[3,"C"], [4,"D"]]
+#
+# with open('result.txt', 'w') as f:
+#     for product in all_products:
+#         f.write("%s\n" % product)
